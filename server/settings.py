5962bd0bc9cfb57ef547bf7fcac402754f3e97e8
gemolina <gabriel.molina@wom.cl>
2024-12-16 03:19:15 -0300

    removing whitenoise

--- server/settings.py
+++ server/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'product',
     'vendor',
     'cart',
