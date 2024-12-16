a11bdd17b1a4c105a337cff12920bb7b78609e25
gemolina <gabriel.molina@wom.cl>
2024-12-16 07:47:01 -0300

    fafa

--- server/settings.py
+++ server/settings.py
@@ -139,5 +139,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
