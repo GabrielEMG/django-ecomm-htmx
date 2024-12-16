e05175af71dd4783bba4b5b33c44aebd6116c52d
gemolina <gabriel.molina@wom.cl>
2024-12-16 04:17:37 -0300

    changing staticfilesdirs

--- server/settings.py
+++ server/settings.py
@@ -123,9 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",  # Define this for a global static directory
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
